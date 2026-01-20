277d65dca6d389a23330291506775cd97a79d087
Shelley Sugiharto <shelleysugiharto@Shelleys-MacBook-Air.local>
2026-01-20 14:09:11 -0600

    using dTree, then validation. Moving to another model

--- Intro.py
+++ Intro.py
@@ -59,5 +59,44 @@ print(X.head())
 print("Predictions:", predictions)
 print("Exact prices:", exact_prices) #results will =predictions because houses are already priced
 
+'''
+Model Validation:
+Starting with Mean Absolute Error (MAE)...
+error = actual - predicted
+MAE is the avg distance between the actual and predicted values.
+'''
+from sklearn.metrics import mean_absolute_error
+mae = mean_absolute_error(exact_prices, predictions) #using js head
+print("Mean Absolute Error (MAE):", mae) #would be 0 in this case because the data is static and exact
+
+#Again, but with the full dataset
+predictions_full = melbourne_model.predict(X)
+mae_full = mean_absolute_error(y, predictions_full)
+print("Mean Absolute Error (MAE) with full dataset:", mae_full)
+
+'''
+in-sample score was done above. This can provide inaccurate results.
+To avoid this in a straightforward fashion, we can exclude some data from the model-building process.
+
+Then the excluded data can be used for validation (validation dataset).
+'''
+from sklearn.model_selection import train_test_split
+
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=1)
+
+melbourne_model = DecisionTreeRegressor()
+melbourne_model.fit(X_train, y_train)
+
+val_predictions = melbourne_model.predict(X_valid)
+mae_val = mean_absolute_error(y_valid, val_predictions)
+
+print("Mean Absolute Error (MAE) with validation dataset:", mae_val)
+
+'''
+The model is not very accurate.
+We can try to improve it by using a more complex model or by tuning the hyperparameters.
+Underfitting and overfitting:
 
 
+We will move on to a more complex model: Random Forest.
+'''
