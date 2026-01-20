7215f6d8a9e23f271d59bbb42287f5fd0d0f6f63
Shelley Sugiharto <shelleysugiharto@Shelleys-MacBook-Air.local>
2026-01-20 11:55:54 -0600

    added intro file

---
+++ Intro.py
@@ -0,0 +1,17 @@
+'''
+Reminder:
+1. Create a new branch
+git checkout -b my-feature-branch
+
+2. Make changes to your files
+echo "Some changes" >> myfile.txt
+
+3. Stage your changes
+git add myfile.txt
+
+4. Commit your changes
+git commit -m "Add some changes"
+
+5. Push your changes to the remote repository
+git push origin my-feature-branch   
+'''
